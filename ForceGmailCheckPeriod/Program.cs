4bc2914284090a2336ed5300bc5dd5af2e5b980e
swebber <scott@teagan.org>
2016-03-19 10:44:29 -0400

    Initial check-in.

---
+++ ForceGmailCheckPeriod/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Threading;
+
+namespace ForceGmailCheckPeriod
+{
+    internal static class Program
+    {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+
+            logger.Info("Application {0}, Version={1} Start", assembly.GetName().Name, fvi.ProductVersion);
+
+            // get application GUID as defined in AssemblyInfo.cs
+            string appGuid = ((GuidAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), false).GetValue(0)).Value.ToString();
+
+            // unique id for global mutex - Global prefix means it is global to the machine
+            string mutexId = string.Format("Global\\{{{0}}}", appGuid);
+
+            // Need a place to store a return value in Mutex() constructor call
+            bool createdNew;
+
+            // edited by Jeremy Wiebe to add example of setting up security for multi-user usage
+            // edited by 'Marc' to work also on localized systems (don't use just "Everyone")
+            var allowEveryoneRule = new MutexAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), MutexRights.FullControl, AccessControlType.Allow);
+            var securitySettings = new MutexSecurity();
+            securitySettings.AddAccessRule(allowEveryoneRule);
+
+            // edited by MasonGZhwiti to prevent race condition on security settings via VanNguyen
+            using (var mutex = new Mutex(false, mutexId, out createdNew, securitySettings))
+            {
+                // edited by acidzombie24
+                var hasHandle = false;
+                try
+                {
+                    try
+                    {
+                        // note, you may want to time out here instead of waiting forever
+                        // edited by acidzombie24
+                        // mutex.WaitOne(Timeout.Infinite, false);
+                        hasHandle = mutex.WaitOne(5000, false);
+                        if (hasHandle == false)
+                        {
+                            logger.Error("Timeout waiting for exclusive access");
+                            throw new TimeoutException("Timeout waiting for exclusive access");
+                        }
+                    }
+                    catch (AbandonedMutexException)
+                    {
+                        // Log the fact the mutex was abandoned in another process, it will still get aquired
+                        hasHandle = true;
+                    }
+
+                    // Perform your work here.
+                    var p = new Processor();
+                    p.Run();
+                }
+                finally
+                {
+                    // edited by acidzombie24, added if statemnet
+                    if (hasHandle)
+                        mutex.ReleaseMutex();
+                }
+            }
+
+            logger.Info("Application {0}, Version={1} End", assembly.GetName().Name, fvi.ProductVersion);
+        }
+    }
+}
